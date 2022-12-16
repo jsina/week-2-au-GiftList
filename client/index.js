b44bddc5c8ab206629d53073426c210014ac7fc7
jsina <jsina.maleki@gmail.com>
2022-12-16 13:33:39 +0330

    solve challenge

--- client/index.js
+++ client/index.js
@@ -1,14 +1,20 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+const merkleTree = new MerkleTree(niceList);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
+  // TODO: how do we prove to the server we're on the nice list?
+  let name = "Norman Block";
+  const index = niceList.find((i) => i == name);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof,
   });
 
   console.log({ gift });
